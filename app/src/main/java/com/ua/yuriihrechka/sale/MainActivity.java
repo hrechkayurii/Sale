fb070e57913c6c28f822e0e2e12e7092f5158d01
ukropBandera <ukrop.bandera@gmail.com>
2019-02-15 21:48:46 +0200

    1

--- app/src/main/java/com/ua/yuriihrechka/sale/MainActivity.java
+++ app/src/main/java/com/ua/yuriihrechka/sale/MainActivity.java
@@ -1,14 +1,38 @@
 package com.ua.yuriihrechka.sale;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private Button joinNowButton, loginButton;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void init() {
+
+        joinNowButton = (Button)findViewById(R.id.main_join_now_btn);
+        loginButton = (Button)findViewById(R.id.main_login_btn);
+
     }
 }
