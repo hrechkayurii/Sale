15a26cb6bf2830dbf0d801ed06f35a6ddb54b6df
ukropBandera <ukrop.bandera@gmail.com>
2019-02-18 23:30:27 +0200

    12

--- app/src/main/java/com/ua/yuriihrechka/sale/AdminAddNewProductActivity.java
+++ app/src/main/java/com/ua/yuriihrechka/sale/AdminAddNewProductActivity.java
@@ -2,11 +2,20 @@ package com.ua.yuriihrechka.sale;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class AdminAddNewProductActivity extends AppCompatActivity {
 
     private String categoryName;
+    private Button addNewProductButton;
+    private EditText inputProductName;
+    private EditText inputProductDescription;
+    private EditText inputProductPrice;
+    private ImageView inputProductImage;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +24,17 @@ public class AdminAddNewProductActivity extends AppCompatActivity {
 
         categoryName = getIntent().getExtras().get("category").toString();
 
-        Toast.makeText(this, categoryName, Toast.LENGTH_LONG).show();
+        init();
+
+
+    }
+
+    private void init() {
+
+        addNewProductButton = (Button)findViewById(R.id.add_new_product);
+        inputProductName = (EditText)findViewById(R.id.product_name);
+        inputProductDescription = (EditText)findViewById(R.id.product_description);
+        inputProductPrice = (EditText)findViewById(R.id.product_price);
+        inputProductImage = (ImageView)findViewById(R.id.select_product_image);
     }
 }
