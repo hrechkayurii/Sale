c529b358b6ee110746699fb25baefe716a185aad
ukropBandera <ukrop.bandera@gmail.com>
2019-02-23 10:45:05 +0200

    22

--- app/src/main/java/com/ua/yuriihrechka/sale/ProductDetailsActivity.java
+++ app/src/main/java/com/ua/yuriihrechka/sale/ProductDetailsActivity.java
@@ -1,14 +1,20 @@
 package com.ua.yuriihrechka.sale;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.cepheuen.elegantnumberbutton.view.ElegantNumberButton;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -17,15 +23,22 @@ import com.google.firebase.database.ValueEventListener;
 
 import com.squareup.picasso.Picasso;
 import com.ua.yuriihrechka.sale.Model.Products;
+import com.ua.yuriihrechka.sale.Prevalent.Prevalent;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.HashMap;
+
 
 public class ProductDetailsActivity extends AppCompatActivity {
 
     //private FloatingActionButton addToCartBtn;
     private ImageView productImage;
-    private ElegantNumberButton namberBtn;
+    private ElegantNumberButton numberBtn;
     private TextView productPrice;
     private TextView productDescription;
     private TextView productName;
+    private Button addToCartBtn;
 
     private String productID = "";
 
@@ -37,6 +50,68 @@ public class ProductDetailsActivity extends AppCompatActivity {
         init();
 
 
+        addToCartBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                addingToCartList();
+            }
+        });
+
+
+    }
+
+    private void addingToCartList() {
+
+        String saveCurrentTime, saveCurrentDate;
+
+        Calendar calForDate = Calendar.getInstance();
+        SimpleDateFormat currentDate = new SimpleDateFormat("MMM dd, yyyy");
+        saveCurrentDate = currentDate.format(calForDate.getTime());
+
+
+        SimpleDateFormat currentTime = new SimpleDateFormat("HH:mm:ss a");
+        saveCurrentTime = currentTime.format(calForDate.getTime());
+
+        final DatabaseReference cartListRef = FirebaseDatabase.getInstance().getReference().child("CartList");
+
+        final HashMap<String, Object> cartMap = new HashMap<>();
+        cartMap.put("pid", productID);
+        cartMap.put("name", productName.getText().toString());
+        cartMap.put("price", productPrice.getText().toString());
+        cartMap.put("date", saveCurrentDate);
+        cartMap.put("time", saveCurrentTime);
+        cartMap.put("quantity", numberBtn.getNumber());
+        cartMap.put("discount", "");
+
+        cartListRef.child("UserView").child(Prevalent.currentOnlineUser.getPhone())
+                .child("Product").child(productID)
+                .updateChildren(cartMap)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+
+                        if (task.isSuccessful()){
+
+                            cartListRef.child("AdminView").child(Prevalent.currentOnlineUser.getPhone())
+                                    .child("Product").child(productID)
+                                    .updateChildren(cartMap)
+                                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+
+                                            if (task.isSuccessful()){
+                                                Toast.makeText(ProductDetailsActivity.this, "Successful add to cart", Toast.LENGTH_LONG).show();
+
+                                                Intent intent = new Intent(ProductDetailsActivity.this, HomeActivity.class);
+                                                startActivity(intent);
+                                            }
+                                        }
+                                    });
+
+                        }
+                    }
+                });
 
 
     }
@@ -75,10 +150,11 @@ public class ProductDetailsActivity extends AppCompatActivity {
 
         //addToCartBtn = (FloatingActionButton)findViewById(R.id.add_product_to_card_btn);
         productImage = (ImageView)findViewById(R.id.product_image_details);
-        namberBtn = (ElegantNumberButton)findViewById(R.id.number_btn);
+        numberBtn = (ElegantNumberButton)findViewById(R.id.number_btn);
         productPrice = (TextView)findViewById(R.id.product_price_details);
         productDescription = (TextView)findViewById(R.id.product_description_details);
         productName = (TextView)findViewById(R.id.product_name_details);
+        addToCartBtn = (Button) findViewById(R.id.add_product_to_card_btn);
 
         getProductDetails(productID);
     }
